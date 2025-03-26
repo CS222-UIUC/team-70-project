e358ed479ddfd33ebfaf5fad96bf24cc26ab4171
Andrew Chen <superandrewchen11@gmail.com>
2025-03-26 13:46:37 -0500

    Testing Githook on Frontend

--- frontend/tests/sum.test.js
+++ frontend/tests/sum.test.js
@@ -2,5 +2,6 @@ const base_dir = '../';
 const sum = require(base_dir + 'sum');
 
 test('adds 1 + 2 to equal 3', () => {
+    // Dummy comment change to test githook
     expect(sum(1, 2)).toBe(3);
 })
